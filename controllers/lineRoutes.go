1a2efd116a5f437acb8e33725f21dcfb07b56701
dre4success <dre4success@live.com>
2024-05-01 17:38:51 +0100

    feat: function to map station ids

--- controllers/lineRoutes.go
+++ controllers/lineRoutes.go
@@ -18,11 +18,11 @@ type routes struct {
 }
 
 type stations struct {
-	Id        string `json:"id"`
-	Name      string `json:"name"`
-	Lat       float64    `json:"lat"`
-	Lon       float64    `json:"lon"`
-	StationId string `json:"stationId"`
+	Id        string  `json:"id"`
+	Name      string  `json:"name"`
+	Lat       float64 `json:"lat"`
+	Lon       float64 `json:"lon"`
+	StationId string  `json:"stationId"`
 }
 
 type stopPointSequences struct {
@@ -30,8 +30,8 @@ type stopPointSequences struct {
 }
 
 type stopPoint struct {
-	ParentId  string `json:"parentId"`
-	StationId string `json:"stationId"`
+	TopMostParentId string `json:"topMostParentId"`
+	StationId       string `json:"stationId"`
 }
 
 func (tl TflLines) Routes(w http.ResponseWriter, r *http.Request) {
@@ -47,10 +47,31 @@ func (tl TflLines) Routes(w http.ResponseWriter, r *http.Request) {
 
 	var lineRoutes routes
 	err = json.Unmarshal(data, &lineRoutes)
+	mapStationIDs(&lineRoutes)
 	if err != nil {
 		log.Println("Error unmarshalling JSON:", err)
 		http.Error(w, "Server error", http.StatusInternalServerError)
 	}
 
 	tl.LineRoutes.Execute(w, lineRoutes)
+}
+
+// geting the stationIds as some stations have IDs that are different from stationIds.
+// The stationIds become useful when we want to get stop arrivals, as they can be passed
+// to a stop arrival endpoint as the naptanId.
+func mapStationIDs(routesData *routes) {
+	stationIDMap := make(map[string]string)
+	for _, stSeq := range routesData.StopPointSequences {
+		for _, sp := range stSeq.StopPoint {
+			stationIDMap[sp.TopMostParentId] = sp.StationId
+		}
+	}
+
+	// map station Ids to the top level stations
+	for i, station := range routesData.Stations {
+		stationID, ok := stationIDMap[station.Id]
+		if ok {
+			routesData.Stations[i].StationId = stationID
+		}
+	}
 }
