1e062c8790619bf0e03138027de24959337d46ef
dre4success <dre4success@live.com>
2024-05-01 10:16:53 +0100

    feat: add linestring data

--- static/js/map.js
+++ static/js/map.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
   const stationsDataElement = document.getElementById('station-data')
   const stationsData = stationsDataElement.getAttribute('data-stations')
+  const lineStringsData = stationsDataElement.getAttribute('data-linestrings')
 
   const stations = JSON.parse(stationsData)
+  let lineStrings = JSON.parse(lineStringsData)
+
+  lineStrings = lineStrings.map((line) =>
+    JSON.parse(line)
+      .flat()
+      .map(([lon, lat]) => [lat, lon])
+  )
 
   const initialMapPosition = stations[0]
     ? [stations[0].lat, stations[0].lon]
@@ -15,6 +23,9 @@ document.addEventListener('DOMContentLoaded', function () {
     attribution:
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map)
+
+  const polyline = L.polyline(lineStrings, { color: 'red' }).addTo(map)
+  map.fitBounds(polyline.getBounds())
 
   stations.forEach((station) => {
     L.marker([station.lat, station.lon]).addTo(map).bindPopup(station.name)
